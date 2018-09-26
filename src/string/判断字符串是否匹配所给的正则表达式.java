c5895a94112a6a7d93ce616dc7b022a88cc2b1b2
gcc <xdugucc@sina.com>
2018-09-26 09:38:49 +0800

    9.26

--- src/string/判断字符串是否匹配所给的正则表达式.java
+++ src/string/判断字符串是否匹配所给的正则表达式.java
@@ -1,11 +1,11 @@
 package string;
+//https://leetcode.com/problems/regular-expression-matching/description/
 
 //p是pattern，.代表一个任何的字符，*代表0个或多个前一个字符
 //暴力法不好做阿，因为*能表示的情况太多了，也许当*为1个的时候不行，为0个的时候就行了，时间复杂度o（n）应该作不来
 
 //考虑用动态规划，那种二维矩阵
 
-//TODO
 public class 判断字符串是否匹配所给的正则表达式 {
     public boolean isMatch(String s, String p) {
         if (s == null || p == null) {
@@ -17,7 +17,7 @@ public class 判断字符串是否匹配所给的正则表达式 {
         valid[0][0] = true;
 
         //对第一行初始化，下面要用，第一行不涉及s串，所以*的情形只有0个。
-        for (int i = 1; i < s.length(); i++) {
+        for (int i = 1; i < p.length(); i++) {
             if (p.charAt(i) == '*' && valid[0][i - 1]) {
                 //*代表0个之前的字符
                 valid[0][i+1] = true;
