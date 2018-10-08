e2082e3bbb424dde17c6bf6476a5c243b7b0b660
gcc <xdugucc@sina.com>
2018-10-08 21:52:16 +0800

    10.8 coding first working day

---
+++ src/array/搜寻旋转后排序数组中元素二分法.java
@@ -0,0 +1,48 @@
+package array;
+
+/**
+ * https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+ * Input: nums = [4,5,6,7,0,1,2], target = 0
+ * Output: 4
+ */
+public class 搜寻旋转后排序数组中元素二分法 {
+    /**
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int left = 0;
+        int right = nums.length-1;
+        while (left <= right) {
+            int mid = (left + right)/2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] > nums[right]) {
+                //左半边是升序的
+                if (target < nums[mid] && target >= nums[left]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                //右半边是升序的
+                if (target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new 搜寻旋转后排序数组中元素二分法().search(new int[]{1,3}, 3));
+    }
+}
