def226c16bb54208521fb053f0f6305fee5066ff
gcc <xdugucc@sina.com>
2018-10-12 10:25:25 +0800

    10.12

--- src/array/最大蓄水坐标差乘高度.java
+++ src/array/最大蓄水坐标差乘高度.java
@@ -1,7 +1,10 @@
 package array;
 
 /*
- * @Description 一边从左移动，一边从右移动，选择更小的那一边向内移动，如果移动之后元素高度更大了，就是一个更优的结构
+ * @Description https://leetcode.com/problems/container-with-most-water/description/
+ * Input: [1,8,6,2,5,4,8,3,7]
+    Output: 49
+ * 一边从左移动，一边从右移动，选择更小的那一边向内移动，如果移动之后元素高度更大了，就是一个更优的结构
  * 。移动大的那边没用。
  * @Date 下午9:19 18-9-6
  * @Param
