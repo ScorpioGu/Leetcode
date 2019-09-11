022c18720c6fd1cd353cce74e49722d50501117c
xdugucc <xdugucc@sina.com>
2019-09-11 19:45:45 +0800

    a

--- src/Main.java
+++ src/Main.java
@@ -2,77 +2,59 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<Service> list = new ArrayList<>();
-        int[] disks = {5, 2, 5, 10};
-        int[] memes = {1, 3, 2, 4};
-        int[] values = {1000, 3000, 15000, 16000};
-        for (int i = 0; i < 4; i++) {
-            list.add(new Service(disks[i], memes[i], values[i]));
-        }
-        System.out.println(solution(15, 10, list));
+        System.out.println(solution(new int[]{4, 2, 7, 6}));
     }
 
-    private static int solution(int totalDisk, int totalMemory, List<Service> services) {
-        int size = services.size();
-        int[] mems = new int[size];
-        int[] disks = new int[size];
-        int[] values = new int[size];
-        for (int i = 0; i < size; i++) {
-            mems[i] = services.get(i).memory;
-            disks[i] = services.get(i).disk;
-            values[i] = services.get(i).user;
+    public static int solution(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return 0;
         }
+        return mergeSort(arr, 0, arr.length - 1);
+    }
 
-        int[][] dp = new int[totalDisk + 1][totalMemory + 1];
-        for(int i = 0 ; i < size ; i++)
-        {
-            for(int u = totalDisk ; u >= disks[i] ;u--) //必须全部从V递减到0
-            {
-                for(int v = totalMemory ; v >= mems[i] ;v--)
-                    dp[u][v] = Math.max(dp[u-disks[i]][v-mems[i]] + values[i] , dp[u][v])  ; //此f[v]实质上是表示的是i-1次之前的值。
+    private static int mergeSort(int[] array, int head, int tail) {
+        if (head == tail) {
+            return 0;
+        }
+        int mid = (head + tail) >> 1;
+        return mergeSort(array, head, mid)
+                + mergeSort(array, mid + 1, tail)
+                + merge(array, head, mid, tail);
+    }
+
+    private static int merge(int[] array, int head, int mid, int tail) {
+        int[] temp = new int[tail - head + 1];
+        int i = head;
+        int j = mid + 1;
+        int k = 0;
+        int res = 0;
+        while (i <= mid && j <= tail) {
+            if (array[i] <= array[j]) {
+                temp[k++] = array[i++];
+            } else {
+                temp[k++] = array[j++];
+                // 到这里array[i] > array[j],
+                // 因为array[head]-array[mid]是排好序的,所以array[i]...array[mid]都>array[j]
+                // 共mid-i+1个
+
+                //下面两句应该意思是一样的,只是角度不同
+                res += mid - i + 1;
+                //res = j - mid;
             }
         }
-        return dp[totalDisk][totalMemory] ;
 
+        while (i <= mid) {
+            temp[k++] = array[i++];
+        }
+
+        while (j <= tail) {
+            temp[k++] = array[j++];
+        }
+
+        for (int l = 0; l < temp.length; l++) {
+            array[l + head] = temp[l];
+        }
+        return res;
     }
-
-
-    private static class Service {
-        int disk;
-        int memory;
-        int user;
-
-        public Service(int disk, int memory, int user) {
-            this.disk = disk;
-            this.memory = memory;
-            this.user = user;
-        }
-
-        public int getDisk() {
-
-            return disk;
-        }
-
-        public void setDisk(int disk) {
-            this.disk = disk;
-        }
-
-        public int getMemory() {
-            return memory;
-        }
-
-        public void setMemory(int memory) {
-            this.memory = memory;
-        }
-
-        public int getUser() {
-            return user;
-        }
-
-        public void setUser(int user) {
-            this.user = user;
-        }
-    }
-
 }
 
