abf0c3860e9d005429b525f8f2beba881bcf56e4
xdugucc <xdugucc@sina.com>
2019-09-11 20:24:55 +0800

    a

--- src/Main.java
+++ src/Main.java
@@ -6,41 +6,26 @@ public class Main {
     }
 
     public static int solution(int[] arr) {
-        List<Integer> A = new ArrayList<>();
-        for (int i = 0; i < arr.length; i++) {
-            A.add(arr[i]);
+        int[] z = Arrays.copyOf(arr, arr.length);
+        Arrays.sort(z);
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < z.length; i++) {
+            map.put(z[i], i);
         }
-        int count = 0;
-        Map<Integer,Integer> map = new HashMap<Integer, Integer>();
-        List<Integer> B = new ArrayList<>(A);
-        Collections.sort(B);
-        for(int i = 0; i < B.size();i++){
-            map.put(B.get(i), i);
-        }
-        //DFS
-        ArrayList<Integer> C = new ArrayList<>(A);
-        ArrayList<Integer> D = new ArrayList<>();
-        for(int i = 0; i < C.size();i++){
-            if(D.contains(C.get(i)))continue;
-            int cycleSize = 1;
-            if(C.get(i)!=B.get(i)){
-                D.add(C.get(i));
-                int a = C.get(i);
-                while(true){
-                    int curA = map.get(a);//当前元素的正确位置
-                    int num = C.get(curA);//当前元素的正确位置上实际的元素
-                    int curNum = map.get(num);//当前元素的正确位置上的实际元素的正确位置(是否能形成cycle)
-                    cycleSize++;
-                    D.add(num);
-                    if(curNum==i){
-                        break;
-                    }
-                    a=num;
+        int loops = 0;//循环节个数
+        boolean[] flag = new boolean[arr.length];
+        //找出循环节的个数
+        for (int i = 0; i < arr.length; i++){
+            if (!flag[i]) {//已经访问过的位置不再访问
+                int j = i;
+                while (!flag[j]) {
+                    flag[j] = true;
+                    j = map.get(arr[j]);
                 }
-                count+=(cycleSize-1);
+                loops++;
             }
         }
-        return count;
+        return arr.length - loops;
 
     }
 }
