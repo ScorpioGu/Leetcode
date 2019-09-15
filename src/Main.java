7b3d313f951404473c35ed2844135da1ac5ecc5b
xdugucc <xdugucc@sina.com>
2019-09-15 20:23:30 +0800

    a

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@
 import java.util.*;
 
 public class Main {
+    static int el = 500;
+    static int er = -500;
+    static int eu = -500;
+    static int ed = 500;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -11,7 +15,13 @@ public class Main {
         Point end = new Point(x, y);
         Set<Point> stones = new HashSet<>();
         for (int i = 0; i < n; i++) {
-            stones.add(new Point(sc.nextInt(), sc.nextInt()));
+            int curx = sc.nextInt();
+            int cury = sc.nextInt();
+            stones.add(new Point(curx, cury));
+            el = Math.min(el, curx);
+            er = Math.max(er, curx);
+            eu = Math.max(eu, cury);
+            ed = Math.min(ed, cury);
         }
         Set<Point> his = new HashSet<>();
         LinkedList<Point> queue = new LinkedList<>();
@@ -27,26 +37,33 @@ public class Main {
                     return;
                 }
                 size--;
-                Point left = new Point(p.x - 11, p.y);
-                if (!stones.contains(left) && !his.contains(left)) {
+                Point left = new Point(p.x - 1, p.y);
+                if (!stones.contains(left) && !his.contains(left) && inArea(left.x, left.y)) {
                     queue.offer(left);
                 }
                 Point right = new Point(p.x + 1, p.y);
-                if (!stones.contains(right) && !his.contains(right)) {
+                if (!stones.contains(right) && !his.contains(right) && inArea(right.x, right.y)) {
                     queue.offer(right);
                 }
                 Point down = new Point(p.x, p.y - 1);
-                if (!stones.contains(down) && !his.contains(down)) {
+                if (!stones.contains(down) && !his.contains(down) && inArea(down.x, down.y)) {
                     queue.offer(down);
                 }
                 Point up = new Point(p.x, p.y + 1);
-                if (!stones.contains(up) && !his.contains(up)) {
+                if (!stones.contains(up) && !his.contains(up) && inArea(up.x, up.y)) {
                     queue.offer(up);
                 }
                 his.add(p);
             }
             path++;
         }
+    }
+
+    private static boolean inArea(int x, int y) {
+        if (x < el - 1 || x > er + 1 || y < ed - 1 || y > eu + 1) {
+            return false;
+        }
+        return true;
     }
 
 }
