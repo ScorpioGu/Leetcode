6b4af297a823317b67c83126734e153dd7822558
xdugucc <xdugucc@sina.com>
2019-09-16 21:21:08 +0800

    a

--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,31 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int K = Integer.parseInt(sc.nextLine());
-        String[] ss = sc.nextLine().split(" ");
+        String[] ss = sc.nextLine().split(",");
         int[] nums = new int[ss.length];
         for (int i = 0; i < nums.length; i++) {
             nums[i] = Integer.parseInt(ss[i]);
         }
-        int N = nums.length;
-        Arrays.sort(nums);
-        int ans = nums[N-1] - nums[0];
 
-        for (int i = 0; i < nums.length - 1; ++i) {
-            int a = nums[i], b = nums[i+1];
-            int high = Math.max(nums[N-1] - K, a + K);
-            int low = Math.min(nums[0] + K, b - K);
-            ans = Math.min(ans, high - low);
+        System.out.println(checkPossibility(nums) ? 1 : 0);
+    }
+
+    public static boolean checkPossibility(int[] nums) {
+        int count = 0;
+        for (int i = 1; i < nums.length && count <= 1; i++) {
+            //当出现前大后小时，一般时更希望把前面的元素变小而不是把后面的元素变大。因为后面元素变大的话，可能会导致之后的序列不递增
+            //但是也有可能时类似于4,5,2这种情况，如果把5改成2会造成422不满足非递减
+            //所以对于这种情况，只能改成455。改动前还是改动后，根据nums[i]与nums[i-2]的大小关系决定
+            if (nums[i] < nums[i - 1]) {
+                count++;
+                if (i - 2 < 0 || nums[i] > nums[i - 2]) {
+                    nums[i - 1] = nums[i];
+                } else {
+                    nums[i] = nums[i - 1];
+                }
+            }
         }
-        System.out.println(ans);
+        return count <= 1;
     }
 }
 
